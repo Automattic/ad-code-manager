7e0674e2c7eb0e36b38caf384313023509b356f3
Daniel Bachhuber <d@danielbachhuber.com>
2012-01-02 18:32:26 -0800

    Initial commit: skeleton of the plugin

---
+++ ad-code-manager.php
@@ -0,0 +1,127 @@
+<?php
+/*
+Plugin Name: Ad Code Manager
+Plugin URI: http://automattic.com
+Description: Easy ad code management 
+Author: Daniel Bachhuber, Rinat Khaziev, Automattic
+Version: 0.0
+Author URI: http://automattic.com
+
+GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+define( 'AD_CODE_MANAGER_VERSION', '0.0' );
+define( 'AD_CODE_MANAGER_ROOT' , dirname(__FILE__) );
+define( 'AD_CODE_MANAGER_FILE_PATH' , AD_CODE_MANAGER_ROOT . '/' . basename(__FILE__) );
+define( 'AD_CODE_MANAGER_URL' , plugins_url( plugin_basename(dirname(__FILE__)).'/') );
+
+class Ad_Code_Manager
+{
+
+	var $ad_tags = array();
+	var $script_url_whitelist = array();
+	
+	/**
+	 * Instantiate the plugin
+	 *
+	 * @since ??
+	 */
+	function __construct() {
+
+		add_action( 'init', array( &$this, 'action_init' ) );
+		add_action( 'admin_init', array( &$this, 'action_admin_init' ) );
+		
+	}
+
+	/**
+	 * Code to run on WordPress' 'init' hook
+	 *
+	 * @since ??
+	 */
+	function action_init() {
+
+		// Allow new domains to be whitelisted
+
+
+		// Ad tags are only run on the frontend
+		if ( !is_admin() ) {
+			require_once AD_CODE_MANAGER_ROOT . '/template-tags.php';
+			add_action( 'acm_tag', array( &$this, 'action_acm_tag' ) );
+		}
+		
+	}
+
+	/**
+	 * Code to run on WordPress' 'admin_init' hook
+	 *
+	 * @since ??
+	 */
+	function action_admin_init() {
+
+		// @todo conditionally load the admin interface if that's enabled
+		// The admin interface should be enabled by a filter and off by default
+		// We'll need additional methods for:
+		// - Displaying the interface
+		// - Saving the data
+		// - Loading the ad codes in the database and registering them
+		// with the plugin using 
+		
+	}
+
+	/**
+	 * Register an ad tag with the plugin so it can be used
+	 * on the frontend of the site
+	 *
+	 * @since ???
+	 *
+	 * @param string $tag Ad tag for this instance of code
+	 * @param string $script URL for ad tag
+	 * @param array $where WordPress-style conditionals for where this code should be displayed
+	 * @param int $priority What priority this registration runs at
+	 * @param array $url_vars Replace tokens in $script with these values
+	 * @return bool|WP_Error $success Whether we were successful in registering the ad tag
+	 */
+	function register_ad_tag( $tag, $script, $where = array(), $priority = 10, $url_vars = array() ) {
+
+		// @todo Run $script aganist a whitelist to make sure it's a safe URL
+		// @todo Sanitize all of the other input
+
+		// @todo logic for saving the ad tag to $this->ad_tags so it's available to $this->action_acm_tag()
+	} 
+
+	/**
+	 * Display the ad code based on what's registered
+	 * and complicated sorting logic
+	 *
+	 * @uses do_action( 'acm_tag, 'your_tag_id' )
+	 */
+	function action_acm_tag( $tag_id ) {
+		
+		// @todo possibly complicated logic for determining which
+		// script is executed while factoring in:
+		// - where it should be displayed
+		// - priority against other ad tags
+
+		// @todo Parse the script URL and replace with any $url_vars
+
+		echo '<script type="text/javascript" src="' . esc_url( $tag_url ) . '"></script>';
+
+	}
+
+}
+global $ad_code_manager;
+$ad_code_manager = new Ad_Code_Manager();
