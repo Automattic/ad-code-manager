59540d8a04f8e9c5081b17523f87b4d43252f237
Kailey Lampert <trepmal@gmail.com>
2021-06-04 10:59:39 -0700

    update tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,11 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	exit( 1 );
 }
 
 require_once $_tests_dir . '/includes/functions.php';
